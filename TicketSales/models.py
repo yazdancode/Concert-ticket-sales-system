c6f9d39bbe12bdf18ad7d5196f2bb929cef7d50f
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-09-21 10:15:35 -0700

    new code version

--- TicketSales/models.py
+++ TicketSales/models.py
@@ -141,3 +141,39 @@ class TimeSlot(models.Model):
             )
         self.full_clean()
         super().save(*args, **kwargs)
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100, verbose_name="نام")
+    family = models.CharField(max_length=100, verbose_name="نام خانوادگی")
+
+    STATUS_CHOICES = [
+        ("Man", "مرد"),
+        ("Woman", "زن"),
+    ]
+
+    gender = models.CharField(
+        max_length=6,
+        choices=STATUS_CHOICES,
+        verbose_name="جنسیت",
+    )
+
+    birth_date = models.DateField(verbose_name="تاریخ تولد", null=True, blank=True)
+    email = models.EmailField(verbose_name="آدرس ایمیل", unique=True, max_length=100)
+    phone_number = models.IntegerField(
+        max_length=15, verbose_name="شماره تلفن", null=True, blank=True
+    )
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", verbose_name="عکس پروفایل", null=True, blank=True
+    )
+    bio = models.TextField(verbose_name="بیوگرافی", blank=True, max_length=255)
+    address = models.CharField(
+        max_length=255, verbose_name="آدرس", null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ثبت‌ نام")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="تاریخ آخرین بروزرسانی"
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.family} - {self.gender}"
