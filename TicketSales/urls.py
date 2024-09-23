01b0e8f63030a3de6813e872fd0091bf6f58252f
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-09-23 13:29:13 -0700

    TicketSales

--- TicketSales/urls.py
+++ TicketSales/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from TicketSales.views import ConcertListView
+from TicketSales.views import ConcertListView, LocationListView
 
 urlpatterns = [
-    path('concert/list/', ConcertListView, name='concert')
+    path('concert/list/', ConcertListView.as_view(), name='concert'),
+    path('location/list/', LocationListView.as_view(), name='location')
 ]
--- TicketSales/views.py
+++ TicketSales/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-from TicketSales.models import Concert
+from django.views.generic import ListView
+from TicketSales.models import Concert, Location
 
-def ConcertListView(request):
-    concert = Concert.objects.all()
-    context = {
-        'concerts': concert,
-        'concertcount': concert.count(),
-    }
-    return render(request, 'TicketSales/concert_list.html', context)
+class ConcertListView(ListView):
+    model = Concert
+    template_name = 'TicketSales/concert_list.html'
+    context_object_name = 'concerts'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['concertcount'] = self.get_queryset().count()
+        return context
+    
+class LocationListView(ListView):
+    model = Location
+    template_name = 'TicketSales/locationlist.html'
+    context_object_name = 'locations'
+    paginate_by = 10
+    
+    
+    
+
+
+
