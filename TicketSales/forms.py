68d8e9193f0b780595427ef25af644f935d05fea
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-10-02 01:09:58 -0700

    file

--- TicketSales/forms.py
+++ TicketSales/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from TicketSales.models import Concert
 
 
@@ -10,4 +11,27 @@ class ConcertForm(forms.ModelForm):
     class Meta:
         model = Concert
         fields = ["name", "singer_name", "length", "Concert_picture"]
-        # exclude = ["Concert_picture"]
+
+    def __init__(self, *args, **kwargs):
+        super(ConcertForm, self).__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs.update(
+            {"class": "mylable", "placeholder": "نام کنسرت"}
+        )
+        self.fields["singer_name"].widget.attrs.update(
+            {"class": "mylable", "placeholder": "نام خواننده"}
+        )
+        self.fields["length"].widget.attrs.update(
+            {"class": "mylable", "placeholder": "مدت زمان"}
+        )
+        self.fields["Concert_picture"].widget.attrs.update({"class": "mylable"})
+
+    # def clean_Concert_picture(self):
+    #     image = self.cleaned_data.get("Concert_picture")
+    #     if image:
+    #         if image.size > 5 * 1024 * 1024:
+    #             raise forms.ValidationError("سایز عکس باید کمتر از 5 مگابایت باشد.")
+    #         if not image.content_type in ["image/jpeg", "image/png"]:
+    #             raise forms.ValidationError(
+    #                 "فایل باید یک عکس با فرمت JPEG یا PNG باشد."
+    #             )
+    #         return image
